5430abe13168714b3898844d5e1bf9b1a80180a6
Moundier <garamoundier.developer@protonmail.com>
2024-11-16 00:08:34 -0300

    chore: minor configuration changes!

---
+++ rag/setup_env.sh
@@ -0,0 +1,10 @@
+
+python -m venv venv; 
+source venv/bin/activate;
+pip install -r requirements.txt
+
+# Ollama at /usr/local
+
+# curl -fsSL https://ollama.com/install.sh | sh
+# ollama pull nomic-embed-text;
+# ollama pull mistral
