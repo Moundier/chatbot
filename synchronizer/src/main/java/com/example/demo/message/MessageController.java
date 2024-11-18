3867dbf05d69def020a2e3bd819eb70c5d6405be
Moundier <garamoundier.developer@protonmail.com>
2024-11-18 00:46:53 -0300

    chore: rabbit consumer and code quality!

--- synchronizer/src/main/java/com/example/demo/message/MessageController.java
+++ synchronizer/src/main/java/com/example/demo/message/MessageController.java
@@ -7,16 +7,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.scheduler.SchedulerService;
+
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/message")
 @RequiredArgsConstructor
 public class MessageController {
+
+    private final SchedulerService schedulerService;
   
     @PostMapping
     public ResponseEntity<Message> receiveMessage(@RequestBody Message message) {
-        System.out.println("Received message: " + message);
+        // System.out.println("Received message: " + message);
+        this.schedulerService.process(message);
         return new ResponseEntity<>(message, HttpStatus.OK);
     }
 }
