5ec4983e9200f59b1195e602ba3bc2924abfa2e1
Moundier <garamoundier.developer@protonmail.com>
2024-11-18 01:20:47 -0300

    refactor: correct queue names!

--- wpp/app/modules/rabbitmq/rabbitmq.ts
+++ wpp/app/modules/rabbitmq/rabbitmq.ts
@@ -16,8 +16,8 @@ export const getChannel: (() => Promise<Channel>) = (async (): Promise<Channel> 
         let connection: Connection = await mqConnect();
 
         const channel: Channel = await connection.createChannel();
-        await channel.assertQueue('request_sync_queue', { durable: true });
-        await channel.assertQueue('response_rag_queue', { durable: true });
+        await channel.assertQueue('request_queue', { durable: true });
+        await channel.assertQueue('response_queue', { durable: true });
 
         return channel;
 
