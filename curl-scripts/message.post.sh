0277a75137a71b6ce709fa9fe72a2b181ddc3b0c
Moundier <garamoundier.developer@protonmail.com>
2024-11-21 01:54:20 -0300

    refactor: change post request body!

--- curl-scripts/message.post.sh
+++ curl-scripts/message.post.sh
@@ -1,21 +1,69 @@
 curl -X POST http://localhost:8080/message \
      -H "Content-Type: application/json" \
-     -d '{"phoneNumber": "1234", "message": "Eai cara!"}'
+     -d '{
+           "id": "some-id",
+           "content": "E aí, cara.",
+           "from": "sender-id",
+           "to": "receiver-id",
+           "timestamp": 1690137600000,
+           "phoneNumber": "1234",
+           "possibleNames": {
+               "name": "John Doe",
+               "shortName": "John",
+               "pushname": "JD"
+           }
+         }'
 
 sleep 1
 
 curl -X POST http://localhost:8080/message \
      -H "Content-Type: application/json" \
-     -d '{"phoneNumber": "1234", "message": "Cade tu?"}' 
+     -d '{
+           "id": "some-id",
+           "content": "Cadê tu?",
+           "from": "sender-id",
+           "to": "receiver-id",
+           "timestamp": 1690137600000,
+           "phoneNumber": "1234",
+           "possibleNames": {
+               "name": "John Doe",
+               "shortName": "John",
+               "pushname": "JD"
+           }
+         }'
 
 sleep 1
 
 curl -X POST http://localhost:8080/message \
      -H "Content-Type: application/json" \
-     -d '{"phoneNumber": "12345", "message": "Eai cara!"}'
+     -d '{
+           "id": "some-id",
+           "content": "E aí, cara.",
+           "from": "sender-id",
+           "to": "receiver-id",
+           "timestamp": 1690137600000,
+           "phoneNumber": "12345",
+           "possibleNames": {
+               "name": "John Doe",
+               "shortName": "John",
+               "pushname": "JD"
+           }
+         }'
 
 sleep 1
 
 curl -X POST http://localhost:8080/message \
      -H "Content-Type: application/json" \
-     -d '{"phoneNumber": "12345", "message": "Cade tu?"}' 
+     -d '{
+           "id": "some-id",
+           "content": "Cadê tu?",
+           "from": "sender-id",
+           "to": "receiver-id",
+           "timestamp": 1690137600000,
+           "phoneNumber": "12345",
+           "possibleNames": {
+               "name": "John Doe",
+               "shortName": "John",
+               "pushname": "JD"
+           }
+         }'
